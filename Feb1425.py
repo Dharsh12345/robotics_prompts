2df0334c34515ed9428d76971e09bac82eaffe98
Dharsh12345 <Dharshwaths675@stemcollegiate.ca>
2025-02-14 09:26:58 -0700

    Create Feb1425.py

---
+++ Feb1425.py
@@ -0,0 +1,8 @@
+food = input("What type of food do you like?: ")
+plant = input("what type of plant you like:")
+cookingType = input("what is a way to cook something?:")
+burntFood = input("How do you describe burnt food?:")
+householdItem = input("Nmae something in you house?:")
+print()
+print("Tonights dinner")
+print("For dinner you should make", cookingType, food, "with", burntFood, plant, "on a plate of", householdItem)
