22ea314bd4b64b7e647a2f7380065205c4d1f000
Dharsh12345 <Dharshwaths675@stemcollegiate.ca>
2025-02-19 10:57:51 -0700

    Create feb19.py

---
+++ feb19.py
@@ -0,0 +1,10 @@
+print("welcome to your adventure")
+print()
+name = input('what is your name?: ')
+worst_enemy_name = input("what is your worst enemy's name: ")
+superpower=input("what is your superpower?")
+where_you_live=input("where do you live?")
+your_favorite_food=input("what is your favorite food?")
+print("Hello " + name + ", your ability to " + superpower + " will make sure you never have to look at " + worst_enemy_name + " again. Go eat " + your_favorite_food + " as you walk down the streets of " + where_you_live + " and use " + superpower + " for good and not evil!")
+
+# I learned how to store and coed properly
