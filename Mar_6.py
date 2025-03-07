be0c11f49054bd88278196f394d0edd0b1914f3a
Dharsh12345 <Dharshwaths675@stemcollegiate.ca>
2025-03-07 09:21:29 -0700

    Create Mar_6.py

---
+++ Mar_6.py
@@ -0,0 +1,27 @@
+I learned how to type in username and etc...
+print("SECURE LOGIN")
+username = input("Username > ")
+if username == "mark":
+  print("welcome mark!")
+else:
+ print("Go away!") 
+username = input("username > ")
+password = input("password > ")
+if username == "mark" and password == "password":
+  print("Welcome Mark!")
+elif username == "suzanne":
+  print("Hey there Suzanne!")
+else:
+  print("Go away!")
+season = input("what is your favorite season?")
+if season == "spring":
+  print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+   print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn":
+  print("The leaves are changing and the air is crisp. Enjoy!")
+elif season == "winter":
+ print("Stay warm by the fire and watch the snow fall.")
+else:
+  print("I don't know that season. Please try again.")
+  
